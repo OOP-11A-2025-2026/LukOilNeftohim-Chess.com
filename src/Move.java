76f1c80604954a1994cfcb4c1a30681f31c8522d
rangelovkiril <klubomirovrangelov@gmail.com>
2025-12-09 09:36:12 +0200

    Move with bit flags

--- src/Move.java
+++ src/Move.java
@@ -1,16 +1,15 @@
 public class Move{
+    public static final byte FLAG_CASTLE      = 0b00000001; 
+    public static final byte FLAG_EN_PASSANT  = 0b00000010;
+    public static final byte FLAG_PROMOTION   = 0b00000100; 
+    public static final byte FLAG_CHECK       = 0b00001000;
+    public static final byte FLAG_MATE        = 0b00010000;   
+
     public Piece piece;         
-    public byte target;          
-    public boolean capture;     
+    public byte target; 
+    public byte flags;         
     public Piece promotion;     
-    public boolean check;
-    public boolean mate;
-    public boolean enPassant;
-    public boolean castling;
-    public class Disambiguation{
-        public byte file;
-        public byte rank;
-    };
+    public byte disambiguation; 
     
     public Move() {};
 }
